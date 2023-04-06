2e3d80a3c5714718817e138a8cff401bb5491789
akishehara <68475519+akishehara@users.noreply.github.com>
2023-04-06 16:50:40 +0530

    commit-store-backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const stores = require("./data/stores");
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req,res) => {
     res.send("API is running..");
 });
 
-app.get("/api/stores", (req, res) =>{
+app.get("/api/stores", (req, res) => {
     res.json(stores);
 });
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.get('/api/stores/:id', (req, res) =>{
+    const store = stores.find((n) => n._id === req.params.id );
+
+    res.send(store);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
