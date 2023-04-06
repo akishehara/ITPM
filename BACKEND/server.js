6be8e74febd1ccd124737eec43312dfafdfb13de
Ravini Nanayakkara <88665535+ravininanayakkara@users.noreply.github.com>
2023-04-06 14:30:28 +0530

    Commit add .env file and nodemon

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const stores = require("./data/stores");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is running..");
@@ -11,7 +13,9 @@ app.get("/api/stores", (req, res) => {
   res.json(stores);
 });
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 
 //fuel stations-ravini
 const stations = require("./data/stations");
@@ -22,4 +26,10 @@ app.get("/", (req, res) => {
 
 app.get("/api/stations", (req, res) => {
   res.json(stations);
+});
+
+app.get("/api/stations/:id", (req, res) => {
+  const station = stations.find((n) => n._id === req.params.id);
+
+  res.send(station);
 });
