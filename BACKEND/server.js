766c895b2343d45a460bfa5402d969d6200987be
Ravini Nanayakkara <88665535+ravininanayakkara@users.noreply.github.com>
2023-04-06 13:01:39 +0530

    Commit-Fuel-Stations-Backend-Data

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,12 +3,23 @@ const stores = require("./data/stores");
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("API is running..");
+app.get("/", (req, res) => {
+  res.send("API is running..");
 });
 
-app.get("/api/stores", (req, res) =>{
-    res.json(stores);
+app.get("/api/stores", (req, res) => {
+  res.json(stores);
 });
 
 app.listen(5000, console.log("Server started on PORT 5000"));
+
+//fuel stations-ravini
+const stations = require("./data/stations");
+
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+app.get("/api/stations", (req, res) => {
+  res.json(stations);
+});
