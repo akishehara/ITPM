aebbb7deaca3e78692df5edcea01c8d4dcc84c5c
Ravini Nanayakkara <88665535+ravininanayakkara@users.noreply.github.com>
2023-04-07 18:44:31 +0530

    commit  update and delete stations

--- BACKEND/controllers/stationController.js
+++ BACKEND/controllers/stationController.js
@@ -51,4 +51,66 @@ const createStation = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getStations, createStation };
+const getStationById = asyncHandler(async (req, res) => {
+  const station = await Station.findById(req.params.id);
+
+  if (station) {
+    res.json(station);
+  } else {
+    res.status(404).json({ message: "Station not found" });
+  }
+});
+
+const updateStation = asyncHandler(async (req, res) => {
+  const {
+    name,
+    category,
+    address,
+    district,
+    hours,
+    days,
+    image,
+    phone,
+    description,
+  } = req.body;
+
+  const station = await Station.findById(req.params.id);
+
+  if (station) {
+    station.name = name;
+    station.category = category;
+    station.address = address;
+    station.district = district;
+    station.hours = hours;
+    station.days = days;
+    station.image = image;
+    station.phone = phone;
+    station.description = description;
+
+    const updatedStation = await station.save();
+    res.json(updatedStation);
+  } else {
+    res.status(404);
+    throw new Error("Station not found");
+  }
+});
+
+const deleteStation = asyncHandler(async (req, res) => {
+  const station = await Station.findById(req.params.id);
+
+  if (station) {
+    await station.remove();
+    res.json({ message: "Station Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Station not found");
+  }
+});
+
+module.exports = {
+  getStations,
+  createStation,
+  getStationById,
+  updateStation,
+  deleteStation,
+};
--- BACKEND/routes/stationRoutes.js
+++ BACKEND/routes/stationRoutes.js
@@ -2,12 +2,19 @@ const express = require("express");
 const {
   getStations,
   createStation,
+  getStationById,
+  updateStation,
+  deleteStation,
 } = require("../controllers/stationController");
 
 const router = express.Router();
 
 router.route("/").get(getStations);
 router.route("/create").post(createStation);
-//router.route("/:id").get().put().delete();
+router
+  .route("/:id")
+  .get(getStationById)
+  .put(updateStation)
+  .delete(deleteStation);
 
 module.exports = router;
