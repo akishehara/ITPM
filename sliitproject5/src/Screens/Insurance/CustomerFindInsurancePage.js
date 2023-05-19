8c2249d0917ccf6a3adb99d765b5012b0c459939
IT20602178 <88676305+HashiniWee@users.noreply.github.com>
2023-05-19 23:29:53 +0530

    commit_ftn_insurance_screen

---
+++ sliitproject5/src/Screens/Insurance/CustomerFindInsurancePage.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import CustomerFindInsurance from "../../Components/Insurance/CustomerFindInsurance";
+import OwnerCompanyCreate from "../../Components/Insurance/OwnerCompanyCreate";
+
+
+export default class CustomerFindInsurancePage extends Component {
+    render() {
+        return (
+            <div>
+            <CustomerFindInsurance/>
+            </div>
+        )
+    }
+}
