287e7a21447e59d8106bd07f8c7746f65a11e5c9
TharushGitHub <91726707+TharushGitHub@users.noreply.github.com>
2023-05-20 14:26:43 +0530

    Frontend-Garage-Screens

---
+++ sliitproject5/src/Screens/Garage/CreatedServicesPage.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import CreatedServices from "../../Components/Garage/CreatedService";
+
+export default class CreatedServicesPage extends Component {
+    render() {
+        return (
+            <div className='div-main-service'>
+            <CreatedServices/>
+            </div>
+        )
+    }
+}
---
+++ sliitproject5/src/Screens/Garage/GarageHomePage.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import CreateServiceForm from "../../Components/Garage/CreateServiceForm";
+
+export default class GarageHomePage extends Component {
+    render() {
+        return (
+            <div>
+            <CreateServiceForm/>
+            </div>
+        )
+    }
+}
