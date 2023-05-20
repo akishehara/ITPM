6cc68e86ef6cb433db29837b2348ef3abab9b76d
TharushGitHub <91726707+TharushGitHub@users.noreply.github.com>
2023-05-20 14:35:44 +0530

    Frontend-Signln-Screen

---
+++ sliitproject5/src/Screens/SignIn.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Login from "../Components/login_component";
+import Navbar from "../Components/navbar_component";
+import Sign from "../Components/signup-component";
+
+export default class SignPage extends Component {
+    render() {
+        return (
+            <div>
+            <Login/>
+            </div>
+        )
+    }
+}
